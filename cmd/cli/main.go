328a0e90eb480557d5e3514984ee7e89bcb623ce
v <v@macbook.local>
2025-11-25 20:04:35 +0300

    Add network metric

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -5,30 +5,40 @@ import (
 
 	"time"
 
+	"pc_metric/internal/logger"
 	"pc_metric/metrics/cpu"
 	net "pc_metric/metrics/net_int"
 	"pc_metric/metrics/ram"
-	"pc_metric/pkg/logger"
 )
 
 func main() {
+	startApp := time.Now()
+	durationWork := 1 * time.Minute
 
 	err := logger.InitLogger()
 	if err != nil {
 		panic(err)
 	}
 	defer logger.Close()
+	//net.DebugInterfaces()
 
 	logger.SystemMessage("=== Start getting CPU & RAM metric ===")
+	logger.SystemMessage("=== Initialization... The network interface speed will be available in 10 seconds ===")
 
-	for i := 0; i < 50; i++ {
+	for time.Since(startApp) < durationWork {
+		_, _, _, netMsg, err := net.NetMetric()
+		if err != nil {
+			fmt.Println("Error", err)
+
+		}
 
 		la := cpu.GetLoadAverage()
 		r := ram.GetMemInfo()
-		net.NetMetric()
-		message := fmt.Sprintf("Load average is: 1 min: %.2f, 5 min: %.2f, 15 min: %.2f | RAM: %v/%vGB (%vGB free) | Net interface name: %s, Send byte: %d, Recive byte: %d", la.Load1, la.Load5, la.Load15, r[0], r[1], r[2], net.IntName, net.SendByte, net.RecByte)
+
+		message := fmt.Sprintf("Load average is: 1 min: %.2f, 5 min: %.2f, 15 min: %.2f | RAM: %v/%vGB (%vGB free) | NET: %s ", la.Load1, la.Load5, la.Load15, r[0], r[1], r[2], netMsg)
 
 		logger.LogMetric(message)
+
 		time.Sleep(1 * time.Second)
 	}
 
--- metrics/net_int/net_int.go
+++ metrics/net_int/net_int.go
@@ -1,31 +1,107 @@
 package net_int
 
 import (
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
 	"github.com/shirou/gopsutil/v4/net"
 )
 
 var (
-	IntName  string
-	SendByte uint64
-	RecByte  uint64
+	IntName string
+
+	prevSend uint64 //для вычисления разницы
+	prevRecv uint64
+
+	initialized bool
+	startTime   time.Time
+	lastTime    time.Time
 )
 
-func NetMetric() {
-	metric, _ := net.IOCounters(true)
+// DetectActiveInterface ищет активный сетевой интерфейс (игнорирует lo)
+func AutoDetectInterface() (string, error) {
+	metrics, err := net.IOCounters(true)
+	if err != nil {
+		return "", err
+	}
+	var activeInterface string
+	var maxTraffic uint64
 
-	for _, stat := range metric {
-		if stat.Name == "en0" {
-			IntName = stat.Name
-		} else if stat.BytesSent > 0 && stat.BytesRecv > 0 {
-			SendByte = stat.BytesSent
-			RecByte = stat.BytesRecv
+	for _, stat := range metrics {
+		if stat.Name == "lo" ||
+			strings.HasPrefix(stat.Name, "awdl") ||
+			strings.HasPrefix(stat.Name, "llw") ||
+			strings.HasPrefix(stat.Name, "bridge") ||
+			strings.HasPrefix(stat.Name, "p2p") {
+			continue
 		}
 
-		switch stat.Name {
+		traffic := stat.BytesRecv + stat.BytesSent
+		if traffic > maxTraffic {
+			maxTraffic = traffic
+			activeInterface = stat.Name
+		}
+	}
+	if activeInterface == "" {
+		return "", errors.New("Not found active interface")
+	}
+	return activeInterface, nil
+}
 
-		case "en0":
+// NetMetric возвращает скорость интерфейса в KB/s.
+func NetMetric() (upKBps, downKBps uint64, readyNow bool, msg string, err error) {
 
+	if IntName == "" {
+		iface, err := AutoDetectInterface()
+		if err != nil {
+			return 0, 0, false, "", err
+		}
+		IntName = iface
+	}
+
+	metric, err := net.IOCounters(true)
+	if err != nil {
+		return 0, 0, false, "", err
+	}
+
+	for _, stat := range metric {
+		if stat.Name == IntName {
+
+			if !initialized {
+				prevSend = stat.BytesSent
+				prevRecv = stat.BytesRecv
+				startTime = time.Now()
+				lastTime = startTime
+				initialized = true
+				return 0, 0, false, "", nil // сразу возвращаем 0 скорости
+			}
+
+			// если прошло меньше 10 секунд — тоже возвращаем 0
+			if time.Since(startTime) < 10*time.Second {
+				return 0, 0, false, "Initialization...", nil
+			}
+
+			//Вычисление скорости канала
+
+			elapsed := time.Since(lastTime).Seconds()
+			if elapsed <= 0 {
+				elapsed = 1
+			}
+
+			upKBps = uint64(float64(stat.BytesSent-prevSend) / 1024 / elapsed)
+			downKBps = uint64(float64(stat.BytesRecv-prevRecv) / 1024 / elapsed)
+
+			prevSend = stat.BytesSent
+			prevRecv = stat.BytesRecv
+			lastTime = time.Now()
+
+			msg := fmt.Sprintf("UP: %d KB/s DOWN: %d KB/s", upKBps, downKBps)
+
+			return upKBps, downKBps, true, msg, nil
 		}
 
 	}
+	return 0, 0, false, "", errors.New("Interface not found")
 }
