4b17b8964dcc0a0aab45c96e5b1cdf104a5eb587
v <v@macbook.local>
2025-12-30 13:12:54 +0300

    Revert "add Makefile"
    This reverts commit bacc3dfbf66bd7c77ee39e1daa77a424519542fd.

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -14,11 +14,75 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// func main() {
+// 	//Загрузка файла переменных окружения
+// 	if err := godotenv.Load(); err != nil {
+// 		fmt.Println("File .env not found")
+// 	}
+
+// 	//стандартное  время работы приложения 1минута, если не передано иное
+// 	timeWorkDefault := flag.Duration("d", 1*time.Minute, "Default time for metrics monitoring - 1 min") // -d 2m, -d 30s, -d 1h
+
+// 	flag.Parse()
+
+// 	if *timeWorkDefault <= 0 {
+// 		fmt.Println("Duration must be > 0")
+// 		os.Exit(1)
+// 	}
+
+// 	ticker := time.NewTicker(1 * time.Second) // как часто собирать метрики
+// 	defer ticker.Stop()
+
+// 	timer := time.NewTimer(*timeWorkDefault) // общее время работы программы
+// 	defer timer.Stop()
+
+// 	//done := make(chan struct{})
+
+// 	err := logger.InitLogger()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer logger.Close()
+
+// 	logger.SystemMessage("=== Start getting CPU & RAM metric ===")
+// 	logger.SystemMessage("=== Initialization... The network interface speed will be available in 10 seconds ===")
+
+// 	// go func() {
+// 	// 	<-timer.C
+// 	// 	close(done)
+// 	// }()
+
+// 	for {
+// 		select {
+// 		case <-ticker.C:
+
+// 			_, _, _, netMsg, err := net.NetMetric()
+// 			if err != nil {
+// 				fmt.Println("Error", err)
+
+// 			}
+
+// 			la := cpu.GetLoadAverage()
+// 			r := ram.GetMemInfo()
+
+// 			message := fmt.Sprintf("Load average is: 1 min: %.2f, 5 min: %.2f, 15 min: %.2f | RAM: %v/%vGB (%vGB free) | NET: %s ", la.Load1, la.Load5, la.Load15, r[0], r[1], r[2], netMsg)
+
+// 			logger.LogMetric(message)
+
+// 		case <-timer.C:
+// 			logger.SystemMessage("\n=== END ===")
+// 			fmt.Println("Exit")
+// 			return
+// 		}
+
+// 	}
+
+// }
+
 func main() {
 	//Загрузка файла переменных окружения
 	if err := godotenv.Load(); err != nil {
 		fmt.Println("File .env not found")
-		os.Exit(1)
 	}
 	cfg := database.Config{
 		Host:     os.Getenv("DB_HOST"),
@@ -70,7 +134,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	ticker := time.NewTicker(*timeMetricDefault) // как часто собирать метрики
+	ticker := time.NewTicker(1 * *timeMetricDefault) // как часто собирать метрики
 	defer ticker.Stop()
 
 	timer := time.NewTimer(*timeWorkDefault) // общее время работы программы
